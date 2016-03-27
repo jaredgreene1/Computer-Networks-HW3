a194e85c3d56502df2d908bbb3877e1f32bb3661
Anonymous <anonymous@noemail.com>
2016-03-27 14:12:48 +0000

    Initial Commit

--- Client.java
+++ Client.java
@@ -12,7 +12,8 @@ public class Client {
     		String hostName = args[0];
      	int portNumber = Integer.parseInt(args[1]);
 
-		try(
+//Authentication & Socket creation
+     try(
       	Socket chatSocket = new Socket(hostName, portNumber);
       	PrintWriter out =
                new PrintWriter(chatSocket.getOutputStream(), true);
@@ -21,16 +22,44 @@ public class Client {
                    new InputStreamReader(chatSocket.getInputStream()));
            BufferedReader stdIn =
                new BufferedReader(
-                    new InputStreamReader(System.in)));
-           String serverMess;
-     		  String userInput;
-     			while ((serverMsg = in.readLine()) != null) {
-                out.println(serverMsg);
-                System.out.println(in.readLine()); //Is this necessary? One off on the readline?
-            }
-     			userInput = stdIn.readLine();
-     			out.write
-     		
+                    new InputStreamReader(System.in))
+   		)
+     {
+				boolean loggedIn = false; //1 when logged in successfully
      
-   }
+ 	 	    	String serverMess;
+     			String userInput;
+				while ((serverMsg = in.readLine()) != null) //intro message for login info 
+      	   	System.out.println(serverMsg); 
+     			out.println(stdIn.readLine());
+     			System.out.print(in.readLine());
+     			out.println(stdIn.readLine());
+      	 	loggedIn = (boolean)in.readLine();
+     			while ((serverMsg = in.readLine()) != null) //message for repeated login info
+      	   	System.out.println(in.readLine());
+    	 while(!loggedIn) //if they never get the correct answer server will close connection - might have an error issue here
+    	 {
+     			out.println(stdIn.readLine());
+     			System.out.print(in.readLine());
+     			out.println(stdIn.readLine());
+     		  	loggedIn = (boolean)in.readLine();
+    	 }
+     
+     //Now you're logged in
+     
+     while((serverMsg = in.readLine() != null))
+           System.out.println(serverMsg);
+     }
+           
+     
+     	catch(UnknownHostException e) {
+        System.out.print("Cannot recognize specified host - system exiting...");
+        System.exit(1);
+      }
+     		
+  		catch(IOException e){
+        System.out.print("Had trouble establishing an IO connection with " + hostName);
+        System.exit(1);
+      }   		
+     
 }
--- Server.java
+++ Server.java
@@ -1,6 +1,24 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server {
-  	public static void main(String[] args)
-}
+  	public static void main(String[] args){
+     try(
+       	FileReader lI = new FileReader("/user_pass.txt");
+       	BufferedReader loginText = new BufferedReader(lI);
+       	HashMap<String, String> loginMap = new HashMap();
+       	String pwd;
+       	String uName = "test";
+       	while((cred = loginText.nextLine()) != null)
+       		loginMap.put(uName, pwd);){}
+     
+     catch(FileIOException e){
+       System.out.print("Login file not found");
+       System.exit(1);
+     }
+         
+           
+           
+         }
+   }
