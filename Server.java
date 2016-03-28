32a0753804f1d29d0ee3ca562af6094db0f0910e
jaredgreene1 <jmg2227@columbia.edu>
2016-03-27 21:07:57 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,5 +1,4 @@
 public class Server {
-<<<<<<< HEAD
   	public static void main(String[] args){
      try
 	{
@@ -23,36 +22,9 @@ public class Server {
 	} 
      catch(FileNotFoundException e)
 	 {
-=======
-        public static void main(String[] args){
-     try
-        {
-        FileReader lI = new FileReader("user_pass.txt");
-        BufferedReader loginText = new BufferedReader(lI);
-        HashMap<String, String> loginMap = new HashMap<String, String>();
-        String pwd;
-        String uName = "test";
-        while((pwd = loginText.readLine()) != null)
-                {
-                //Revers the hash entry when you actually end up parsing.
-//                      System.out.println(pwd);
-                        loginMap.put(pwd, uName);
-//                      System.out.println("Size is now " + loginMap.size());
-                }
-
-//Check hashmap print
-//              for(String entry: loginMap.values())
-//                 System.out.println("* " + entry + " *");
-
-        } 
-     catch(FileNotFoundException e)
-         {
->>>>>>> 96759055bb82fc2b848ebe715347f2b56ba2d09f
-       System.out.println("Login file not found");
-       System.exit(1);
-     }
-
-<<<<<<< HEAD
+	    System.out.println("Login file not found");
+            System.exit(1);
+         }
 	 catch(IOException e)
 	 {
 		 System.out.println("input error");
@@ -61,13 +33,3 @@ public class Server {
 
   }
 }
-=======
-         catch(IOException e)
-         {
-                 System.out.println("input error");
-                 System.exit(1);
-         }
-
-  }
-}
->>>>>>> 96759055bb82fc2b848ebe715347f2b56ba2d09f
